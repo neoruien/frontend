3b5a97aee1ee6f5f06c0d0cd40f2eff3d7cc8839
successs404 <neoruien@u.nus.edu>
2021-01-09 07:06:38 +0800

    Add action whenever an item is removed from box

--- src/ui/itemDataScreen.js
+++ src/ui/itemDataScreen.js
@@ -1,9 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Text, View, Image, TouchableOpacity } from 'react-native';
+import ACTIONS from '../../data/actionData.js';
 import { styles } from "../../stylesheets/appStyles.js";
+import action from '../models/action.js';
 
-export default function addBoxScreen({ navigation }) { 
+export default function itemDataScreen({ navigation }) { 
   return (
     <View style={styles.mainContainer}>
       <View style={styles.tallTopContainer}>
@@ -18,7 +20,10 @@ export default function addBoxScreen({ navigation }) {
         <Text style={styles.formText}>Name: </Text>
         <Text style={styles.formText}>Description: </Text>
         <Text style={styles.formText}>Box added to: </Text>
-        <TouchableOpacity onPress={() => navigation.navigate('Box data')} style={styles.buttonBorder}>
+        <TouchableOpacity onPress={() => {
+          ACTIONS.push(new action("An item is removed"));
+          navigation.navigate('Box data');
+        }} style={styles.buttonBorder}>
           <Text style={styles.buttonText}>Remove item</Text>
         </TouchableOpacity>
       </View>
