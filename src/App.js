938f2bed615a71ce28bca11be152e1262c7bfc7b
ryanlimjr <ryanlimjr@gmail.com>
2021-01-08 12:31:53 +0800

    create navigation screen template

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { styles } from "../stylesheets/appStyles.js";
+
+function homeScreen({ navigation }) { 
+  return (
+    <View style={styles.container}>
+      <Button
+        title = 'Add box'
+        onPress={() => navigation.navigate('details')}
+      />
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+function detailsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={homeScreen} /> 
+        <Stack.Screen name="details" component={detailsScreen} /> 
+      </Stack.Navigator> 
+    </NavigationContainer>
+  )
+
+}
+
