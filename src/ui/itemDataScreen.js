8d82d827dbfe701abe67290eab3bf54ff3449747
successs404 <neoruien@u.nus.edu>
2021-01-08 20:33:47 +0800

    Edit itemDataScreen

--- src/ui/itemDataScreen.js
+++ src/ui/itemDataScreen.js
@@ -1,13 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image, TouchableOpacity } from 'react-native';
 import { styles } from "../../stylesheets/appStyles.js";
 
 export default function addBoxScreen({ navigation }) { 
   return (
-    <View style={styles.container}>
-      <Text>itemData</Text>
-      <StatusBar style="auto" />
+    <View style={styles.mainContainer}>
+      <View style={styles.tallTopContainer}>
+        <Image
+            style={styles.bigimage}
+            source={require("../../assets/Bag.jpg")}
+        />
+      </View>
+
+      {/* My Boxes container */}
+      <View style={styles.shortPageContainer}>
+        <Text style={styles.formText}>Name: </Text>
+        <Text style={styles.formText}>Description: </Text>
+        <Text style={styles.formText}>Box added to: </Text>
+        <TouchableOpacity onPress={() => navigation.navigate('Box data')} style={styles.buttonBorder}>
+          <Text style={styles.buttonText}>Remove item</Text>
+        </TouchableOpacity>
+      </View>
+
     </View>
   );
 }
